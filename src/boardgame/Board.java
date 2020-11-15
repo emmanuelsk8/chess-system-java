2b53e7a4bff8a22cdb6544932f7466c6071a3382
Emmanuel H. Tamagnini <emmanuel.tamagnini@gmail.com>
2020-11-15 11:07:52 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -35,6 +35,17 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) throw new BoardException("Position not on the board");
+		if(piece(position) == null) return null;
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	//Verifica se a posição existe no tabuleiro
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
