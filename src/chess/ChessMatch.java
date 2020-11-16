f83280fecb8a80fbfcd1c46b52d5ad071b1c056c
Emmanuel H. Tamagnini <emmanuel.tamagnini@gmail.com>
2020-11-16 20:54:37 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,6 +23,13 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	// Move a peça da posição de origem para a de destino
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -20,6 +20,5 @@ public abstract class ChessPiece extends Piece {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
-	
+
 }
